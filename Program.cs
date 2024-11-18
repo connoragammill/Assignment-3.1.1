52e5365f31d93d41020d8caf70d0832017f500cf
connor gammill <connor.gammill@outlook.com>
2024-11-18 13:49:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Assignment_3._1._1._
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			string evenNumbers = EvenNumbers(); 
+			Console.WriteLine(evenNumbers);
+		}
+		static string EvenNumbers()
+		{
+			StringBuilder sb = new StringBuilder(); 
+			for (int i = 2; i < 100; i += 2) 
+			{
+				sb.Append(i);
+				sb.Append(" "); 
+			}
+
+			return sb.ToString();
+
+		}
+    }
+}
